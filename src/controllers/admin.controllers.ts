c6c79fc50106149211b4273af5a967203726a27a
utkarshpatil-13 <utkarshpatil.it@gmail.com>
2025-01-30 16:14:09 +0530

    update postgresql url

--- src/controllers/admin.controllers.ts
+++ src/controllers/admin.controllers.ts
@@ -1,8 +1,10 @@
 import { AdminService } from '../services/admin.services';
 import { AdminRepository } from '../repositories/admin.repositories';
 
-const adminRepository = new AdminRepository();
-const adminService = new AdminService(adminRepository);
+
+
+// const adminRepository = new AdminRepository();
+// const adminService = new AdminService(adminRepository);
 
 export class AdminController {
 
